c458deb43af63e308d9966e68eb4a71e9416c45e
Elora Elbert <82468024+eloraelbert@users.noreply.github.com>
2022-04-06 22:34:36 +0000

    change id and classes in script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function createNewElement() {
-    // First create a DIV element.
+  // First create a DIV element.
 	var txtNewInputBox = document.createElement('div');
+  txtNewInputBox.classList.add("draggable");
+  //set divCount to # of draggable divs
+  var divcount = document.querySelectorAll('.draggable').length;
+  
+  // Then add the content (a new input box) of the element.
+	txtNewInputBox.innerHTML = "<input draggable = 'true' type='text' id='drag" + 
+    divcount + "' ondragstart='drag(event)' type='text' id='fname' name='fname' value='Create Element' ondblclick='deleteBox();'>";
 
-    // Then add the content (a new input box) of the element.
-	txtNewInputBox.innerHTML = "<input draggable = 'true' type='text' id='drag1' ondragstart='drag(event)' type='text' id='fname' name='fname' value='Create Element' ondblclick='deleteBox();'>";
-
-    // Finally put it where it is supposed to appear.
+  // Finally put it where it is supposed to appear.
 	document.getElementById("inputBoard").appendChild(txtNewInputBox);
 }
+
+//to do 
+//when creating new textbox, change id of input to be drag(num of )
