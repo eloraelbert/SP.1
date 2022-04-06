6cee07fefa583d4024e5c48e59b45d4489b66fb9
Elora Elbert <82468024+eloraelbert@users.noreply.github.com>
2022-04-06 21:41:17 +0000

    double click deletes textboxes added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function createNewElement() {
 	var txtNewInputBox = document.createElement('div');
 
     // Then add the content (a new input box) of the element.
-	txtNewInputBox.innerHTML = "<input draggable = 'true' type='text' id='drag1' ondragstart='drag(event)' type='text' id='fname' name='fname' value='Create Element'>";
+	txtNewInputBox.innerHTML = "<input draggable = 'true' type='text' id='drag1' ondragstart='drag(event)' type='text' id='fname' name='fname' value='Create Element' ondblclick='deleteBox();'>";
 
     // Finally put it where it is supposed to appear.
 	document.getElementById("inputBoard").appendChild(txtNewInputBox);
